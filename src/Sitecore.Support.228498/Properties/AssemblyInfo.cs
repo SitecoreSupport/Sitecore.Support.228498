01c70aecd28aca5667552f7c0d0270c19ffc02a8
DK\EVS <evs@sitecore.com>
2018-07-06 11:35:58 +0300

    initial commit with fix. See commens near "issue #228498"

---
+++ src/Sitecore.Support.228498/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.228498")]
+[assembly: AssemblyProduct("Sitecore.Support.228498")]
+[assembly: ComVisible(false)]
